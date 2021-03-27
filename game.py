2fcd9019d9efe7a9ae35312917c93ebd74208a95
MrDNA <mrdna@MBP.local>
2021-03-27 01:10:06 -0700

    working game

--- game.py
+++ game.py
@@ -16,38 +16,64 @@ def rand_free_coord():
 def rand_2_4():
     return random.choices((2,4), weights=(0.9, 0.1))[0]
 
-def redraw_grid():
+def redraw_grid(grid):
     grid[rand_free_coord()] = rand_2_4()
     grid[rand_free_coord()] = rand_2_4()
     os.system('clear')
-    print(grid)
+    return grid
+
+def strip_zeros(line):
+    line = line[line != 0]
+    zeros = 4 - len(line)
+    for _ in range(zeros):
+        line = np.append(line, 0)
+    return line
+
+def press_line(line):
+    line = strip_zeros(line)
+    for i in range(3):
+        val_1 = line[i]
+        val_2 = line[i + 1]
+        if val_1 == val_2:
+            line[i] = val_1 * 2
+            line[i + 1] = 0    
+    return strip_zeros(line)
+
+def make_move(grid, direction):
+    if direction == 'down':
+        for i in range(4):
+            grid[:, i] = np.flip(press_line(np.flip(grid[:, i])))
+    elif direction == 'up':
+        for i in range(4):
+            grid[:, i] = press_line(grid[:, i])
+    elif direction == 'right':
+        for i in range(4):
+            grid[i, :] = np.flip(press_line(np.flip(grid[i, :])))   
+    elif direction == 'left':
+        for i in range(4):
+            grid[i, :] = press_line(grid[i, :])
+    
+    return(grid)
 
 #Game Loop
 keys = {
-    'up': 'i',
-    'down': 'k',
-    'left': 'j',
-    'right': 'l'
+    'i': 'up',
+    'k': 'down',
+    'j': 'left',
+    'l': 'right'
 }
 
 grid = np.zeros((4,4), dtype=int)
-redraw_grid()
+print(redraw_grid(grid))
 move = ''
 
 while True:
     move = input()
 
-    if move in keys.values():
-        if move == keys['up']:
-            pass
-        elif move == keys['down']:
-            pass
-        elif move == keys['left']:
-            pass
-        elif move == keys['right']:
-            pass
+    if move in keys:
+        grid = make_move(grid, keys[move])
 
-        redraw_grid()
+        print(redraw_grid(grid))
     elif move == 'q':
         exit()
     else:
--- grid_test.py
+++ grid_test.py
@@ -1,8 +1,23 @@
 import numpy as np
 import os
 
+def strip_zeros(line):
+    line = line[line != 0]
+    zeros = 4 - len(line)
+    for _ in range(zeros):
+        line = np.append(line, 0)
+    return line
+
+
+def press_line(line):
+    line = strip_zeros(line)
+    for i in range(3):
+        val_1 = line[i]
+        val_2 = line[i + 1]
+        if val_1 == val_2:
+            line[i] = val_1 * 2
+            line[i + 1] = 0    
+    return strip_zeros(line)
+
 grid = np.random.choice((0,2,4,8), size=(4,4))
-
-os.system('clear')
 print(grid)
-
